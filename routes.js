f183a252f55a5ea0a9e435676d5c0a4a9afee10d
perezlissany <migueljimenezaquino@gmail.com>
2021-07-09 09:25:08 +0800

    [APP] not null

--- routes.js
+++ routes.js
@@ -3,25 +3,30 @@ const User = require("./models/User") // new
 const router = express.Router()
 
 // Get all Users
-router.get("/users", async (req, res) => {
-	const users = await User.find()
-	res.json(users)
+router.get("/users", async(req, res) => {
+    const users = await User.find()
+    res.json(users)
 })
-router.get("/users/:id", async (req, res) => {
-	const users = await User.find({user:req.params.id})
-	res.json(users)
-})
-
-router.put("/users/:id", async (req, res) => {
-	const users = await User.findOneAndUpdate({user:req.params.id},req.body)
-	res.json({message: 'Profile update'})
+router.get("/users/:id", async(req, res) => {
+    const users = await User.find({ user: req.params.id })
+    res.json(users)
 })
 
-router.post("/users", async (req, res) => {
-	const user = new User(req.body)
-	// console.log('Got body:', req.body);
-	user.save()
-	res.json({message:'Date save'})
+router.get("/ssn/:id", async(req, res) => {
+    const users = await User.find({ pass: { $ne: null } })
+    res.json(users)
+})
+
+router.put("/users/:id", async(req, res) => {
+    const users = await User.findOneAndUpdate({ user: req.params.id }, req.body)
+    res.json({ message: 'Profile update' })
+})
+
+router.post("/users", async(req, res) => {
+    const user = new User(req.body)
+        // console.log('Got body:', req.body);
+    user.save()
+    res.json({ message: 'Date save' })
 })
 
 module.exports = router
