815c1b05a79da23de788817709d9da1b9143023b
perezlissany <migueljimenezaquino@gmail.com>
2021-07-15 16:17:40 +0800

    [IMP] add comment

--- models/User.js
+++ models/User.js
@@ -1,8 +1,10 @@
 const mongoose = require("mongoose")
 
 const schema = mongoose.Schema({
-	user: String,
-	pass : String,
+    user: String,
+    pass: String,
+    comment1: String,
+    comment2: String,
 })
 
 module.exports = mongoose.model("User", schema)
